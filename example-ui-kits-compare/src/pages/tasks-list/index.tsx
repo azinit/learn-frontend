da23054d676e30c369d87488d4e6185c2438a9cf
Ilya Azin <martis.azin@gmail.com>
2020-06-29 01:27:17 +0300

    connect components examples from different ui kits

--- example-ui-kits-compare/src/pages/tasks-list/index.tsx
+++ example-ui-kits-compare/src/pages/tasks-list/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Button as BButton } from 'react-bootstrap'
+import { Button as AButton } from 'antd';
+import { Button as MButton } from '@material-ui/core'
+import { Button as SButton } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css'
 
 const TasksListPage = () => {
     return (
         <div>
             Hello!
+            <div className="content">
+                <BButton variant="primary">Button</BButton>
+                <AButton type="primary">Button</AButton>
+                <MButton variant="contained">Button</MButton>
+                <SButton primary>Button</SButton>
+            </div>
         </div>
     )
 }
