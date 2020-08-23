61be9e172ba81f4d19b23c7535af07982010e12f
Ilya Azin <martis.azin@gmail.com>
2020-08-23 14:56:52 +0300

    feat(canvas-api#drawer): add color randomizer

--- examples/canvas-api/src/examples/drawer.ts
+++ examples/canvas-api/src/examples/drawer.ts
@@ -10,13 +10,17 @@ const clearCanvas = () => {
     ctx.fillRect(0, 0, 800, 800);
 }
 
+const randomInt = (min = 0, max = 1) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 let x = 0;
 let y = 0;
 setInterval(() => {
     clearCanvas();
-    ctx.fillStyle = 'cyan';
+    ctx.fillStyle = `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
     ctx.fillRect(x, y, 100, 100);
-    x++;
-    y++;
+    x = (x + 10) % 800
+    y = (y + 2) % 800
 }, 10)
 
