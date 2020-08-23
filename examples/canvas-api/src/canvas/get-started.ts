5975e95c85d088f27582dfbccd48afb96f12b649
Ilya Azin <martis.azin@gmail.com>
2020-08-23 13:46:06 +0300

    feat(canvas-api): add path example

--- examples/canvas-api/src/canvas/get-started.ts
+++ examples/canvas-api/src/canvas/get-started.ts
@@ -1,10 +1,20 @@
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'rgb(200, 0, 0)';
-ctx.fillRect(10, 10, 50, 50);
+// ctx.fillStyle = 'rgb(200, 0, 0)';
+// ctx.fillRect(10, 10, 50, 50);
 
-ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.fillRect(30, 30, 50, 50);
+// ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+// ctx.fillRect(30, 30, 50, 50);
+
+// ctx.strokeRect(0, 0, 30, 60);
+
+// ctx.clearRect(20, 20, 10, 10);
+
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(100, 0);
+ctx.lineTo(100, 100);
+ctx.fill();
 
 document.body.appendChild(canvas);
