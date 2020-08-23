06ac7418f65462606e15db82c38c1a9b1d5fbdc9
Ilya Azin <martis.azin@gmail.com>
2020-08-23 13:53:28 +0300

    feat(canvas-api): add gradient example

--- examples/canvas-api/src/canvas/get-started.ts
+++ examples/canvas-api/src/canvas/get-started.ts
@@ -4,12 +4,12 @@ const ctx = canvas.getContext("2d");
 const renderSquads = () => {
     ctx.fillStyle = 'rgb(200, 0, 0)';
     ctx.fillRect(10, 10, 50, 50);
-    
+
     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
     ctx.fillRect(30, 30, 50, 50);
-    
+
     ctx.strokeRect(0, 0, 30, 60);
-    
+
     ctx.clearRect(20, 20, 10, 10);
 }
 
@@ -46,5 +46,15 @@ const renderPath2D = () => {
     ctx.fill(circle);
 }
 
-renderPath2D();
+const renderGradient = () => {
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+            ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ', ' +
+                Math.floor(255 - 42.5 * j) + ', 0)';
+            ctx.fillRect(j * 25, i * 25, 25, 25);
+        }
+    }
+}
+
+renderGradient();
 document.body.appendChild(canvas);
