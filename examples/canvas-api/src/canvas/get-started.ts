e0e400212db496b3bbef104c59419b3c16ae12b2
Ilya Azin <martis.azin@gmail.com>
2020-08-23 13:42:53 +0300

    feat(canvas-api): enchance get-started example

--- examples/canvas-api/src/canvas/get-started.ts
+++ examples/canvas-api/src/canvas/get-started.ts
@@ -1,7 +1,10 @@
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, 200, 100);
+ctx.fillStyle = 'rgb(200, 0, 0)';
+ctx.fillRect(10, 10, 50, 50);
+
+ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+ctx.fillRect(30, 30, 50, 50);
 
 document.body.appendChild(canvas);
