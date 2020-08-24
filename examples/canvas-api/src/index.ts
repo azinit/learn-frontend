eb38830e6796b15a4b8646d3eb0defb5e3e8b517
Ilya Azin <martis.azin@gmail.com>
2020-08-24 08:47:24 +0300

    feat(canvas-api): minor fix

--- examples/canvas-api/src/index.ts
+++ examples/canvas-api/src/index.ts
@@ -1,4 +1,8 @@
 import "./init";
+// import "./examples/get-started";
+// import "./examples/animations";
+// import "./examples/drawer";
+// import "./examples/snake";
 import "./examples/konvajs";
 import "./index.scss";
 import "./types";
