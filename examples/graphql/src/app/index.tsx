91354b1b668a30bd73e392e53ed948a00d55f5a5
Ilya Azin <martis.azin@gmail.com>
2020-10-03 19:01:44 +0300

    feat(graphql): init structure
    https://github.com/martis-git/learn-frontend/issues/279

---
+++ examples/graphql/src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+        TODO App
+      </header>
+    </div>
+  );
+}
+
+export default App;
