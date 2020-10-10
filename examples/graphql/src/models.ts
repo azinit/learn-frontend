c581ce078a1fe92d0a4733b2dfbfee5ebe3dd2aa
Ilya Azin <martis.azin@gmail.com>
2020-10-10 17:37:38 +0300

    feat(graphql): temp fix off null-check
    https://github.com/martis-git/learn-frontend/issues/279

--- examples/graphql/src/models.ts
+++ examples/graphql/src/models.ts
@@ -13,24 +13,24 @@ export type Scalars = {
 };
 
 export type Query = {
-  readonly _: Maybe<Scalars['Int']>;
-  readonly albums: Maybe<AlbumsPage>;
-  readonly album: Maybe<Album>;
-  readonly comments: Maybe<CommentsPage>;
-  readonly comment: Maybe<Comment>;
-  readonly photos: Maybe<PhotosPage>;
-  readonly photo: Maybe<Photo>;
-  readonly posts: Maybe<PostsPage>;
-  readonly post: Maybe<Post>;
-  readonly todos: Maybe<TodosPage>;
-  readonly todo: Maybe<Todo>;
-  readonly users: Maybe<UsersPage>;
-  readonly user: Maybe<User>;
+  readonly _?: Maybe<Scalars['Int']>;
+  readonly albums?: Maybe<AlbumsPage>;
+  readonly album?: Maybe<Album>;
+  readonly comments?: Maybe<CommentsPage>;
+  readonly comment?: Maybe<Comment>;
+  readonly photos?: Maybe<PhotosPage>;
+  readonly photo?: Maybe<Photo>;
+  readonly posts?: Maybe<PostsPage>;
+  readonly post?: Maybe<Post>;
+  readonly todos?: Maybe<TodosPage>;
+  readonly todo?: Maybe<Todo>;
+  readonly users?: Maybe<UsersPage>;
+  readonly user?: Maybe<User>;
 };
 
 
 export type QueryAlbumsArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
@@ -40,7 +40,7 @@ export type QueryAlbumArgs = {
 
 
 export type QueryCommentsArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
@@ -50,7 +50,7 @@ export type QueryCommentArgs = {
 
 
 export type QueryPhotosArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
@@ -60,7 +60,7 @@ export type QueryPhotoArgs = {
 
 
 export type QueryPostsArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
@@ -70,7 +70,7 @@ export type QueryPostArgs = {
 
 
 export type QueryTodosArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
@@ -80,7 +80,7 @@ export type QueryTodoArgs = {
 
 
 export type QueryUsersArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
@@ -89,27 +89,27 @@ export type QueryUserArgs = {
 };
 
 export type PageQueryOptions = {
-  readonly paginate: Maybe<PaginateOptions>;
-  readonly slice: Maybe<SliceOptions>;
-  readonly sort: Maybe<ReadonlyArray<Maybe<SortOptions>>>;
-  readonly operators: Maybe<ReadonlyArray<Maybe<OperatorOptions>>>;
-  readonly search: Maybe<SearchOptions>;
+  readonly paginate?: Maybe<PaginateOptions>;
+  readonly slice?: Maybe<SliceOptions>;
+  readonly sort?: Maybe<ReadonlyArray<Maybe<SortOptions>>>;
+  readonly operators?: Maybe<ReadonlyArray<Maybe<OperatorOptions>>>;
+  readonly search?: Maybe<SearchOptions>;
 };
 
 export type PaginateOptions = {
-  readonly page: Maybe<Scalars['Int']>;
-  readonly limit: Maybe<Scalars['Int']>;
+  readonly page?: Maybe<Scalars['Int']>;
+  readonly limit?: Maybe<Scalars['Int']>;
 };
 
 export type SliceOptions = {
-  readonly start: Maybe<Scalars['Int']>;
-  readonly end: Maybe<Scalars['Int']>;
-  readonly limit: Maybe<Scalars['Int']>;
+  readonly start?: Maybe<Scalars['Int']>;
+  readonly end?: Maybe<Scalars['Int']>;
+  readonly limit?: Maybe<Scalars['Int']>;
 };
 
 export type SortOptions = {
-  readonly field: Maybe<Scalars['String']>;
-  readonly order: Maybe<SortOrderEnum>;
+  readonly field?: Maybe<Scalars['String']>;
+  readonly order?: Maybe<SortOrderEnum>;
 };
 
 export enum SortOrderEnum {
@@ -118,9 +118,9 @@ export enum SortOrderEnum {
 }
 
 export type OperatorOptions = {
-  readonly kind: Maybe<OperatorKindEnum>;
-  readonly field: Maybe<Scalars['String']>;
-  readonly value: Maybe<Scalars['String']>;
+  readonly kind?: Maybe<OperatorKindEnum>;
+  readonly field?: Maybe<Scalars['String']>;
+  readonly value?: Maybe<Scalars['String']>;
 };
 
 export enum OperatorKindEnum {
@@ -131,177 +131,177 @@ export enum OperatorKindEnum {
 }
 
 export type SearchOptions = {
-  readonly q: Maybe<Scalars['String']>;
+  readonly q?: Maybe<Scalars['String']>;
 };
 
 export type AlbumsPage = {
-  readonly data: Maybe<ReadonlyArray<Maybe<Album>>>;
-  readonly links: Maybe<PaginationLinks>;
-  readonly meta: Maybe<PageMetadata>;
+  readonly data?: Maybe<ReadonlyArray<Maybe<Album>>>;
+  readonly links?: Maybe<PaginationLinks>;
+  readonly meta?: Maybe<PageMetadata>;
 };
 
 export type Album = {
-  readonly id: Maybe<Scalars['ID']>;
-  readonly title: Maybe<Scalars['String']>;
-  readonly user: Maybe<User>;
-  readonly photos: Maybe<PhotosPage>;
+  readonly id?: Maybe<Scalars['ID']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly user?: Maybe<User>;
+  readonly photos?: Maybe<PhotosPage>;
 };
 
 
 export type AlbumPhotosArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 export type User = {
-  readonly id: Maybe<Scalars['ID']>;
-  readonly name: Maybe<Scalars['String']>;
-  readonly username: Maybe<Scalars['String']>;
-  readonly email: Maybe<Scalars['String']>;
-  readonly address: Maybe<Address>;
-  readonly phone: Maybe<Scalars['String']>;
-  readonly website: Maybe<Scalars['String']>;
-  readonly company: Maybe<Company>;
-  readonly posts: Maybe<PostsPage>;
-  readonly albums: Maybe<AlbumsPage>;
-  readonly todos: Maybe<TodosPage>;
+  readonly id?: Maybe<Scalars['ID']>;
+  readonly name?: Maybe<Scalars['String']>;
+  readonly username?: Maybe<Scalars['String']>;
+  readonly email?: Maybe<Scalars['String']>;
+  readonly address?: Maybe<Address>;
+  readonly phone?: Maybe<Scalars['String']>;
+  readonly website?: Maybe<Scalars['String']>;
+  readonly company?: Maybe<Company>;
+  readonly posts?: Maybe<PostsPage>;
+  readonly albums?: Maybe<AlbumsPage>;
+  readonly todos?: Maybe<TodosPage>;
 };
 
 
 export type UserPostsArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
 export type UserAlbumsArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 
 export type UserTodosArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 export type Address = {
-  readonly street: Maybe<Scalars['String']>;
-  readonly suite: Maybe<Scalars['String']>;
-  readonly city: Maybe<Scalars['String']>;
-  readonly zipcode: Maybe<Scalars['String']>;
-  readonly geo: Maybe<Geo>;
+  readonly street?: Maybe<Scalars['String']>;
+  readonly suite?: Maybe<Scalars['String']>;
+  readonly city?: Maybe<Scalars['String']>;
+  readonly zipcode?: Maybe<Scalars['String']>;
+  readonly geo?: Maybe<Geo>;
 };
 
 export type Geo = {
-  readonly lat: Maybe<Scalars['Float']>;
-  readonly lng: Maybe<Scalars['Float']>;
+  readonly lat?: Maybe<Scalars['Float']>;
+  readonly lng?: Maybe<Scalars['Float']>;
 };
 
 export type Company = {
-  readonly name: Maybe<Scalars['String']>;
-  readonly catchPhrase: Maybe<Scalars['String']>;
-  readonly bs: Maybe<Scalars['String']>;
+  readonly name?: Maybe<Scalars['String']>;
+  readonly catchPhrase?: Maybe<Scalars['String']>;
+  readonly bs?: Maybe<Scalars['String']>;
 };
 
 export type PostsPage = {
-  readonly data: Maybe<ReadonlyArray<Maybe<Post>>>;
-  readonly links: Maybe<PaginationLinks>;
-  readonly meta: Maybe<PageMetadata>;
+  readonly data?: Maybe<ReadonlyArray<Maybe<Post>>>;
+  readonly links?: Maybe<PaginationLinks>;
+  readonly meta?: Maybe<PageMetadata>;
 };
 
 export type Post = {
-  readonly id: Maybe<Scalars['ID']>;
-  readonly title: Maybe<Scalars['String']>;
-  readonly body: Maybe<Scalars['String']>;
-  readonly user: Maybe<User>;
-  readonly comments: Maybe<CommentsPage>;
+  readonly id?: Maybe<Scalars['ID']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly body?: Maybe<Scalars['String']>;
+  readonly user?: Maybe<User>;
+  readonly comments?: Maybe<CommentsPage>;
 };
 
 
 export type PostCommentsArgs = {
-  options: Maybe<PageQueryOptions>;
+  options?: Maybe<PageQueryOptions>;
 };
 
 export type CommentsPage = {
-  readonly data: Maybe<ReadonlyArray<Maybe<Comment>>>;
-  readonly links: Maybe<PaginationLinks>;
-  readonly meta: Maybe<PageMetadata>;
+  readonly data?: Maybe<ReadonlyArray<Maybe<Comment>>>;
+  readonly links?: Maybe<PaginationLinks>;
+  readonly meta?: Maybe<PageMetadata>;
 };
 
 export type Comment = {
-  readonly id: Maybe<Scalars['ID']>;
-  readonly name: Maybe<Scalars['String']>;
-  readonly email: Maybe<Scalars['String']>;
-  readonly body: Maybe<Scalars['String']>;
-  readonly post: Maybe<Post>;
+  readonly id?: Maybe<Scalars['ID']>;
+  readonly name?: Maybe<Scalars['String']>;
+  readonly email?: Maybe<Scalars['String']>;
+  readonly body?: Maybe<Scalars['String']>;
+  readonly post?: Maybe<Post>;
 };
 
 export type PaginationLinks = {
-  readonly first: Maybe<PageLimitPair>;
-  readonly prev: Maybe<PageLimitPair>;
-  readonly next: Maybe<PageLimitPair>;
-  readonly last: Maybe<PageLimitPair>;
+  readonly first?: Maybe<PageLimitPair>;
+  readonly prev?: Maybe<PageLimitPair>;
+  readonly next?: Maybe<PageLimitPair>;
+  readonly last?: Maybe<PageLimitPair>;
 };
 
 export type PageLimitPair = {
-  readonly page: Maybe<Scalars['Int']>;
-  readonly limit: Maybe<Scalars['Int']>;
+  readonly page?: Maybe<Scalars['Int']>;
+  readonly limit?: Maybe<Scalars['Int']>;
 };
 
 export type PageMetadata = {
-  readonly totalCount: Maybe<Scalars['Int']>;
+  readonly totalCount?: Maybe<Scalars['Int']>;
 };
 
 export type TodosPage = {
-  readonly data: Maybe<ReadonlyArray<Maybe<Todo>>>;
-  readonly links: Maybe<PaginationLinks>;
-  readonly meta: Maybe<PageMetadata>;
+  readonly data?: Maybe<ReadonlyArray<Maybe<Todo>>>;
+  readonly links?: Maybe<PaginationLinks>;
+  readonly meta?: Maybe<PageMetadata>;
 };
 
 export type Todo = {
-  readonly id: Maybe<Scalars['ID']>;
-  readonly title: Maybe<Scalars['String']>;
-  readonly completed: Maybe<Scalars['Boolean']>;
-  readonly user: Maybe<User>;
+  readonly id?: Maybe<Scalars['ID']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly completed?: Maybe<Scalars['Boolean']>;
+  readonly user?: Maybe<User>;
 };
 
 export type PhotosPage = {
-  readonly data: Maybe<ReadonlyArray<Maybe<Photo>>>;
-  readonly links: Maybe<PaginationLinks>;
-  readonly meta: Maybe<PageMetadata>;
+  readonly data?: Maybe<ReadonlyArray<Maybe<Photo>>>;
+  readonly links?: Maybe<PaginationLinks>;
+  readonly meta?: Maybe<PageMetadata>;
 };
 
 export type Photo = {
-  readonly id: Maybe<Scalars['ID']>;
-  readonly title: Maybe<Scalars['String']>;
-  readonly url: Maybe<Scalars['String']>;
-  readonly thumbnailUrl: Maybe<Scalars['String']>;
-  readonly album: Maybe<Album>;
+  readonly id?: Maybe<Scalars['ID']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly url?: Maybe<Scalars['String']>;
+  readonly thumbnailUrl?: Maybe<Scalars['String']>;
+  readonly album?: Maybe<Album>;
 };
 
 export type UsersPage = {
-  readonly data: Maybe<ReadonlyArray<Maybe<User>>>;
-  readonly links: Maybe<PaginationLinks>;
-  readonly meta: Maybe<PageMetadata>;
+  readonly data?: Maybe<ReadonlyArray<Maybe<User>>>;
+  readonly links?: Maybe<PaginationLinks>;
+  readonly meta?: Maybe<PageMetadata>;
 };
 
 export type Mutation = {
-  readonly _: Maybe<Scalars['Int']>;
-  readonly createAlbum: Maybe<Album>;
-  readonly updateAlbum: Maybe<Album>;
-  readonly deleteAlbum: Maybe<Scalars['Boolean']>;
-  readonly createComment: Maybe<Comment>;
-  readonly updateComment: Maybe<Comment>;
-  readonly deleteComment: Maybe<Scalars['Boolean']>;
-  readonly createPhoto: Maybe<Photo>;
-  readonly updatePhoto: Maybe<Photo>;
-  readonly deletePhoto: Maybe<Scalars['Boolean']>;
-  readonly createPost: Maybe<Post>;
-  readonly updatePost: Maybe<Post>;
-  readonly deletePost: Maybe<Scalars['Boolean']>;
-  readonly createTodo: Maybe<Todo>;
-  readonly updateTodo: Maybe<Todo>;
-  readonly deleteTodo: Maybe<Scalars['Boolean']>;
-  readonly createUser: Maybe<User>;
-  readonly updateUser: Maybe<User>;
-  readonly deleteUser: Maybe<Scalars['Boolean']>;
+  readonly _?: Maybe<Scalars['Int']>;
+  readonly createAlbum?: Maybe<Album>;
+  readonly updateAlbum?: Maybe<Album>;
+  readonly deleteAlbum?: Maybe<Scalars['Boolean']>;
+  readonly createComment?: Maybe<Comment>;
+  readonly updateComment?: Maybe<Comment>;
+  readonly deleteComment?: Maybe<Scalars['Boolean']>;
+  readonly createPhoto?: Maybe<Photo>;
+  readonly updatePhoto?: Maybe<Photo>;
+  readonly deletePhoto?: Maybe<Scalars['Boolean']>;
+  readonly createPost?: Maybe<Post>;
+  readonly updatePost?: Maybe<Post>;
+  readonly deletePost?: Maybe<Scalars['Boolean']>;
+  readonly createTodo?: Maybe<Todo>;
+  readonly updateTodo?: Maybe<Todo>;
+  readonly deleteTodo?: Maybe<Scalars['Boolean']>;
+  readonly createUser?: Maybe<User>;
+  readonly updateUser?: Maybe<User>;
+  readonly deleteUser?: Maybe<Scalars['Boolean']>;
 };
 
 
@@ -406,8 +406,8 @@ export type CreateAlbumInput = {
 };
 
 export type UpdateAlbumInput = {
-  readonly title: Maybe<Scalars['String']>;
-  readonly userId: Maybe<Scalars['ID']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly userId?: Maybe<Scalars['ID']>;
 };
 
 export type CreateCommentInput = {
@@ -417,9 +417,9 @@ export type CreateCommentInput = {
 };
 
 export type UpdateCommentInput = {
-  readonly name: Maybe<Scalars['String']>;
-  readonly email: Maybe<Scalars['String']>;
-  readonly body: Maybe<Scalars['String']>;
+  readonly name?: Maybe<Scalars['String']>;
+  readonly email?: Maybe<Scalars['String']>;
+  readonly body?: Maybe<Scalars['String']>;
 };
 
 export type CreatePhotoInput = {
@@ -429,9 +429,9 @@ export type CreatePhotoInput = {
 };
 
 export type UpdatePhotoInput = {
-  readonly title: Maybe<Scalars['String']>;
-  readonly url: Maybe<Scalars['String']>;
-  readonly thumbnailUrl: Maybe<Scalars['String']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly url?: Maybe<Scalars['String']>;
+  readonly thumbnailUrl?: Maybe<Scalars['String']>;
 };
 
 export type CreatePostInput = {
@@ -440,8 +440,8 @@ export type CreatePostInput = {
 };
 
 export type UpdatePostInput = {
-  readonly title: Maybe<Scalars['String']>;
-  readonly body: Maybe<Scalars['String']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly body?: Maybe<Scalars['String']>;
 };
 
 export type CreateTodoInput = {
@@ -450,47 +450,47 @@ export type CreateTodoInput = {
 };
 
 export type UpdateTodoInput = {
-  readonly title: Maybe<Scalars['String']>;
-  readonly completed: Maybe<Scalars['Boolean']>;
+  readonly title?: Maybe<Scalars['String']>;
+  readonly completed?: Maybe<Scalars['Boolean']>;
 };
 
 export type CreateUserInput = {
   readonly name: Scalars['String'];
   readonly username: Scalars['String'];
   readonly email: Scalars['String'];
-  readonly address: Maybe<AddressInput>;
-  readonly phone: Maybe<Scalars['String']>;
-  readonly website: Maybe<Scalars['String']>;
-  readonly company: Maybe<CompanyInput>;
+  readonly address?: Maybe<AddressInput>;
+  readonly phone?: Maybe<Scalars['String']>;
+  readonly website?: Maybe<Scalars['String']>;
+  readonly company?: Maybe<CompanyInput>;
 };
 
 export type AddressInput = {
-  readonly street: Maybe<Scalars['String']>;
-  readonly suite: Maybe<Scalars['String']>;
-  readonly city: Maybe<Scalars['String']>;
-  readonly zipcode: Maybe<Scalars['String']>;
-  readonly geo: Maybe<GeoInput>;
+  readonly street?: Maybe<Scalars['String']>;
+  readonly suite?: Maybe<Scalars['String']>;
+  readonly city?: Maybe<Scalars['String']>;
+  readonly zipcode?: Maybe<Scalars['String']>;
+  readonly geo?: Maybe<GeoInput>;
 };
 
 export type GeoInput = {
-  readonly lat: Maybe<Scalars['Float']>;
-  readonly lng: Maybe<Scalars['Float']>;
+  readonly lat?: Maybe<Scalars['Float']>;
+  readonly lng?: Maybe<Scalars['Float']>;
 };
 
 export type CompanyInput = {
-  readonly name: Maybe<Scalars['String']>;
-  readonly catchPhrase: Maybe<Scalars['String']>;
-  readonly bs: Maybe<Scalars['String']>;
+  readonly name?: Maybe<Scalars['String']>;
+  readonly catchPhrase?: Maybe<Scalars['String']>;
+  readonly bs?: Maybe<Scalars['String']>;
 };
 
 export type UpdateUserInput = {
-  readonly name: Maybe<Scalars['String']>;
-  readonly username: Maybe<Scalars['String']>;
-  readonly email: Maybe<Scalars['String']>;
-  readonly address: Maybe<AddressInput>;
-  readonly phone: Maybe<Scalars['String']>;
-  readonly website: Maybe<Scalars['String']>;
-  readonly company: Maybe<CompanyInput>;
+  readonly name?: Maybe<Scalars['String']>;
+  readonly username?: Maybe<Scalars['String']>;
+  readonly email?: Maybe<Scalars['String']>;
+  readonly address?: Maybe<AddressInput>;
+  readonly phone?: Maybe<Scalars['String']>;
+  readonly website?: Maybe<Scalars['String']>;
+  readonly company?: Maybe<CompanyInput>;
 };
 
 export enum CacheControlScope {
